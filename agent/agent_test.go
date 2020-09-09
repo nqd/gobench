7d4a277558fd8454dea96190e1a21efd31bfc65c
nqd <dinh.nguyen@veriksystems.com>
2020-09-09 16:57:50 +0700

    move nop metric logger to test

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -1,11 +1,39 @@
 package agent
 
 import (
+	"context"
 	"testing"
 
 	"github.com/gobench-io/gobench/logger"
+	"github.com/gobench-io/gobench/pb"
 	"github.com/stretchr/testify/assert"
 )
+
+// nop metric logger
+type nopLog struct{}
+
+func (n *nopLog) FindCreateGroup(context.Context, *pb.FCGroupReq) (*pb.FCGroupRes, error) {
+	return nil, nil
+}
+func (n *nopLog) FindCreateGraph(context.Context, *pb.FCGraphReq) (*pb.FCGraphRes, error) {
+	return nil, nil
+}
+func (n *nopLog) FindCreateMetric(context.Context, *pb.FCMetricReq) (*pb.FCMetricRes, error) {
+	return nil, nil
+}
+func (n *nopLog) Histogram(context.Context, *pb.HistogramReq) (*pb.HistogramRes, error) {
+	return nil, nil
+}
+func (n *nopLog) Counter(context.Context, *pb.CounterReq) (*pb.CounterRes, error) {
+	return nil, nil
+}
+func (n *nopLog) Gauge(context.Context, *pb.GaugeReq) (*pb.GaugeRes, error) {
+	return nil, nil
+}
+
+func newNopMetricLog() *nopLog {
+	return &nopLog{}
+}
 
 func newAgent(t *testing.T, opts *Options) *Agent {
 	logger := logger.NewNopLogger()
